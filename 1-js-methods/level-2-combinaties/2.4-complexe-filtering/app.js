53047bcc7a4214b06dbbebec5635c40957bb4ff5
iliavdb <9026087@student.zadkine.nl>
2025-09-15 13:46:40 +0200

    opdracht2

--- 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
+++ 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
@@ -8,14 +8,15 @@ console.log('Alle getallen:', getallen);
 console.log('Alle woorden:', woorden);
 
 // TODO: Filter getallen die groter zijn dan 10 EN kleiner dan 20
-const middelsteGetallen = /* jouw code hier */
+const middelsteGetallen = getallen.filter(num => num > 10 && num < 20);
 console.log('Getallen tussen 10 en 20:', middelsteGetallen);
 
 // TODO: Filter woorden die beginnen met 'a' EN langer zijn dan 5 letters
-const langeAwoorden = /* jouw code hier */
+const langeAwoorden = woorden.filter(woord => woord.toLowerCase().startsWith('a') && woord.length > 5
+);
 console.log('Lange woorden met a:', langeAwoorden);
 
 // TODO: Filter even getallen die groter zijn dan 15
-const groteEvenGetallen = /* jouw code hier */
+const groteEvenGetallen = getallen.filter(num => num > 15 && num % 2 === 0);
 console.log('Grote even getallen:', groteEvenGetallen);
 
